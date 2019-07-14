a562e51c1dcb53e79bde463baf9ad9c738a114fd
John Dunn <jdunn33@cnm.edu>
2019-07-13 18:49:52 -0600

    added initial directory structure, added HTML and Persona information

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang=en>
+	<head>
+		<meta charset="UTF-8">
+		<body>
+		<h1>My PWP John DUNN</h1>
+		<br>
+		<h1>Purpose</h1>
+		<ul>
+			<li>The purpose of my website is to provide a portal for our out of state family to see pictures and stories aobut the granddog Lucy.</li>
+		</ul>
+		<h1>Audience</h1>
+		<ul>
+			<li>The audience would be comprised of our parents and siblings who only get to see Lucy once or twice a year.</li>
+		</ul>
+		<h1>Goal</h1>
+		<ul>
+			<li>My goal is to provide a gallery to entertain and inform our family about our aging Chihuahua.</li>
+		</ul>
+		<h1>Persona</h1>
+		<ul>
+			<li>Becky Benedict-Grandmother</li>
+			<li>Becky is 62 years old and has suffered a traumatic brain injury leaving her unable to work or drive on her own. As a result she is home bound unless someone can driver her</li>
+			<li>Becky likes to keep up with her kids and grand kids on Facebook since all of them live in different cities and states this makes it hard to visit ofter</li>
+			<li>Becky does have an Android smart phone and a tablet she will be using to access my site and keep up with the updates provided for Lucy the Grand dog.</li>
+			<li>Becky's brain injury has left her with a failing memory so she will need to bookmark the site to be able to return to it often since it will be outside of the Facebook framework she is used to.</li>
+			<li>My overarching goal for the site is to bring some happiness and a sense of closeness that she is lacking from her proximity to her daughter by providing these updates.</li>
+		</ul>
+	</body>
+	</head>
+
