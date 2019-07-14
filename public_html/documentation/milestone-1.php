69fde18edbb760855705d2766507d69f2db9a3ac
John Dunn <jdunn33@cnm.edu>
2019-07-13 18:56:36 -0600

    changed punctuation, corrected spelling

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -20,8 +20,8 @@
 		<h1>Persona</h1>
 		<ul>
 			<li>Becky Benedict-Grandmother</li>
-			<li>Becky is 62 years old and has suffered a traumatic brain injury leaving her unable to work or drive on her own. As a result she is home bound unless someone can driver her</li>
-			<li>Becky likes to keep up with her kids and grand kids on Facebook since all of them live in different cities and states this makes it hard to visit ofter</li>
+			<li>Becky is 62 years old and has suffered a traumatic brain injury leaving her unable to work or drive on her own. As a result she is home bound unless someone can driver her.</li>
+			<li>Becky likes to keep up with her kids and grand kids on Facebook since all of them live in different cities and states this makes it hard to visit often.</li>
 			<li>Becky does have an Android smart phone and a tablet she will be using to access my site and keep up with the updates provided for Lucy the Grand dog.</li>
 			<li>Becky's brain injury has left her with a failing memory so she will need to bookmark the site to be able to return to it often since it will be outside of the Facebook framework she is used to.</li>
 			<li>My overarching goal for the site is to bring some happiness and a sense of closeness that she is lacking from her proximity to her daughter by providing these updates.</li>
