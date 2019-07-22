21877df128100872c6d8208c70cdd29abd626e55
John Dunn <jdunn33@cnm.edu>
2019-07-22 11:13:12 -0600

    changed tag placement, added new photo, added colon, changed head tag, changed formatting

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,16 +3,21 @@
 	<head>
 		<title>My PWP John Dunn</title>
 		<meta charset="UTF-8">
-		<body>
+	</head>
+	<body>
 		<h1>MY PWP JOHN DUNN</h1>
 		<br>
 		<h1>Purpose</h1>
 		<ul>
-			<li>The purpose of my website is to provide a portal for our out of state family to see pictures and stories about the grand dog Lucy.</li>
+			<li>The purpose of my website is to provide a portal for our out of state family to see pictures and stories
+				about the grand dog Lucy.
+			</li>
 		</ul>
 		<h1>Audience</h1>
 		<ul>
-			<li>The audience would be comprised of our parents and siblings who only get to see Lucy once or twice a year.</li>
+			<li>The audience would be comprised of our parents and siblings who only get to see Lucy once or twice a
+				year.
+			</li>
 		</ul>
 		<h1>Goal</h1>
 		<ul>
@@ -22,23 +27,43 @@
 		<img src="grandmother.jpg" alt="Becky Benedict" width="350" height="350">
 		<ul>
 			<li>Becky Benedict-Grandmother</li>
-			<li>Becky is 62 years old and has suffered a traumatic brain injury leaving her unable to work or drive on her own. As a result she is home bound unless someone can driver her.</li>
-			<li>Becky likes to keep up with her kids and grand kids on Facebook since all of them live in different cities and states this makes it hard to visit often.</li>
-			<li>Becky does have an Android smart phone and a tablet she will be using to access my site and keep up with the updates provided for Lucy the Grand dog.</li>
-			<li>Becky's brain injury has left her with a failing memory so she will need to bookmark the site to be able to return to it often since it will be outside of the Facebook framework she is used to.</li>
-			<li>My overarching goal for the site is to bring some happiness and a sense of closeness that she is lacking from her proximity to her daughter by providing these updates.</li>
-			</ul>
+			<li>Becky is 62 years old and has suffered a traumatic brain injury leaving her unable to work or drive on her
+				own. As a result she is home bound unless someone can driver her.
+			</li>
+			<li>Becky likes to keep up with her kids and grand kids on Facebook since all of them live in different cities
+				and states this makes it hard to visit often.
+			</li>
+			<li>Becky does have an Android smart phone and a tablet she will be using to access my site and keep up with
+				the updates provided for Lucy the Grand dog.
+			</li>
+			<li>Becky's brain injury has left her with a failing memory so she will need to bookmark the site to be able to
+				return to it often since it will be outside of the Facebook framework she is used to.
+			</li>
+			<li>My overarching goal for the site is to bring some happiness and a sense of closeness that she is lacking
+				from her proximity to her daughter by providing these updates.
+			</li>
+		</ul>
 		<h2>Persona Two: Sister</h2>
 		<img src="renea.jpg" alt="Renea" width="350" height="350">
-			<ul>
-		<li>Sister Renea is a busy teacher who does not get to see us very ofter, she loves the dog and wants a way to keep up with her.</li>
-		<li>She is 50 years old and is a librarian at a public school in Arlington, TX. She likes to travel but doesn't get to New Mexico often.</li>
-		<li>Renea is tech savvy and has a Windows computer that she works on at least 50 hours a week. She consumes most of her web content on this machine.</li>
-		<li>She also has an Android phone and a tablet to browse pictures but does not get cell service in her house which makes sending pictures via text message difficult</li>
-		<li>She recently lost her dog and has not been able to get another, they have been busy beginning to build a house in another state</li>
-		<li>My goal for her for this site is to give her a place to see Lucy and have a smile at her lazy way of life</li>
+		<ul>
+			<li>Sister Renea is a busy teacher who does not get to see us very ofter, she loves the dog and wants a way to
+				keep up with her.
+			</li>
+			<li>She is 50 years old and is a librarian at a public school in Arlington, TX. She likes to travel but doesn't
+				get to New Mexico often.
+			</li>
+			<li>Renea is tech savvy and has a Windows computer that she works on at least 50 hours a week. She consumes
+				most of her web content on this machine.
+			</li>
+			<li>She also has an Android phone and a tablet to browse pictures but does not get cell service in her house
+				which makes sending pictures via text message difficult
+			</li>
+			<li>She recently lost her dog and has not been able to get another, they have been busy beginning to build a
+				house in another state
+			</li>
+			<li>My goal for her for this site is to give her a place to see Lucy and have a smile at her lazy way of life
+			</li>
 		</ul>
-		</head>
 	</body>
 </html>
 
