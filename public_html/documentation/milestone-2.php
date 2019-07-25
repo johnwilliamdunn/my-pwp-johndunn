2f3bcb7b6f0b8f586c3573724516944d8e8ff7e7
John Dunn <jdunn33@cnm.edu>
2019-07-25 17:32:17 -0600

    removed content not needed for 2a

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -15,19 +15,12 @@
 				<h2>About Me</h2>
 			</div>
 
-			<p>I am a stay at home dog that likes long walks at the park, eating cat poop, and barking at the mailman.
-			I was born in the harsh lands of Southern New Mexico and lived the life of a wild Southern Chihuahua.
-			I was found on the side of the road by a benevolent stranger who picked me up and took me into a kennel where I healed up and got stronger.
-			When the kennel could no longer care for me I moved again, this time north to Albuquerque, I ended up in the East Mountains, where its very cold.
-			My new home was called Lap Dog Rescue and I lived there for over a year with a bunch of other lapdogs from around the region.
-			To tell the truth I don't like other dogs very much, I'm afraid they will want to fight me, so I tend to try to attack first.
-			After being at Lapdog Rescue for over a year these Texans called named the Dunn's, they said they wanted to adopt me.
-			If you ask the Dunn's they didn't know I was a Chihuahua, they thought I was some kind of terrier, suckers!
+			<p>sections
+				what sections after that
+				contact form section
 			</p>
 		<img src="images/Lu.jpg" alt = "Lucy at rest";
-				<p>After I went to their tiny house the big surprise was I am a Chihuahua! It tuns out my predecessor was also a Chiweenie so they had some experience with my kind.
-					But I'm a purebred, desert dog, where my predecessor was just a normal junkyard dog. As you can see I am far superior in my begging skills.
-					I hope you enjoy keeping up with my adventures!
+				<p>
 				</p>
 
 	</body>
