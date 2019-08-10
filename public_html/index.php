deb9023ee4de7c38b4cdd3e9364aefd0908fcc6b
John Dunn <jdunn33@cnm.edu>
2019-08-10 09:23:11 -0600

    adding flexbox container and images

--- public_html/index.php
+++ public_html/index.php
@@ -40,16 +40,27 @@
 		</section>
 <!--		adding gallery section-->
 		<div class="row">
+			<!-- begin gallery item -->
 			<div class="col-6 col-lg-3 mb-3">
-				<a href="img/dogcat.jpg" data-fancybox="gallery" data-caption=""this is a cat 001">
-				<img class=""img-fluid">
+				<a href="images/dogcatfull.jpg" data-fancybox="gallery" data-caption="this is Lucy 001">
+					<img src="images/dogcat.jpg" alt="dog 001" class="img-fluid">
 				</a>
 			</div>
-		</div>
-		<!-- Optional JavaScript -->
-		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	</body>
-</html>
+			<!-- begin gallery item -->
+			<div class="col-6 col-lg-3 mb-3">
+				<a href="images/dogshadowfull.jpg" data-fancybox="gallery" data-caption="this is Lucy 002">
+					<img src="images/dogshadow.jpg" alt="dog 002" class="img-fluid">
+				</a>
+			</div>
+			<!-- begin gallery item -->
+			<div class="col-6 col-lg-3 mb-3">
+				<a href="images/dogsunfull.jpg" data-fancybox="gallery" data-caption="this is Lucy 003">
+					<img src="images/dogsun.jpg" alt="dog 003" class="img-fluid">
+				</a>
+			</div>
+			<!-- begin gallery item -->
+			<div class="col-6 col-lg-3 mb-3">
+				<a href="images/dogsandfull.jpg" data-fancybox="gallery" data-caption="this is Lucy 004">
+					<img src="images/dogsand.jpg" alt="dog 004" class="img-fluid">
+				</a>
+			</div>
