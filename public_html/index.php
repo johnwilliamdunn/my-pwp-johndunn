9457d557ed727617eef80ef96565889feed4da26
John Dunn <jdunn33@cnm.edu>
2019-08-12 07:53:59 -0600

    removed jquery

--- public_html/index.php
+++ public_html/index.php
@@ -12,7 +12,6 @@
 <!--		Bootstrap JS-->
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <!--		FancyBox 3-->
@@ -23,14 +22,12 @@
 	<body>
 <!--		adding Page Header-->
 		<header class="bg-primary py-2 text white">
-			<div class="container">
-				<div class="row mx-md-n5">
+			<div class="container"></div>
+			<div class="row mx-md-n5"></div>
 					<div class="col px-md-5"><div class="p-3 bg-primary"></div></div>
 					<div class="col px-md-5"><div class="p-3 bg-primary"></div></div>
-				</div>
 				<span class="border-top"></span>
 				<h2>Lucy's Life</h2>
-			</div>
 		</header>
 <!--		adding content section-->
 		<section class="py-5">
