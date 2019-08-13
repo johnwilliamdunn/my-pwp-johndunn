31740036e3d7bb69d9da052484291ba1ffdd6682
John Dunn <jdunn33@cnm.edu>
2019-08-13 16:57:26 -0600

    removed mail.php

--- public_html/index.php
+++ public_html/index.php
@@ -114,56 +114,72 @@
 			</div>
 			</section>
 <!--		contact form section-->
-		<section class="py-5">
-			<div class="container">
-						<form id="Lucys-contact-form" action="php/mailer.php" method="post">
-							<div class="form-group">
-								<label for="contactLucy">Lucy <span class="text-danger">*</span></label>
-								<div class="input-group">
-									<div class="input-group-prepend">
-										<span class="input-group-text"></span>
-										<i class="fa fa-user" aria-hidden="true"></i>
-									</div>
-									<input type="text" class="form-control" id="contactLucy" name="contactLucy" placeholder="Lucy">
-								</div>
-							</div>
-							<div class="form-group">
-								<label for="contactLucyEmail">Email <span class="text-danger">*</span></label>
-								<div class="input-group">
-									<div class="input-group-prepend">
-										<span class="input-group-text"></span>
-										<i class="fa fa-envelope" aria-hidden="true"></i>
-									</div>
-								</div>
-								<input type="email" class="form-control" id="contactLucyEmail" name="contactLucyEmail" placeholder="Email">
-							</div>
-					</div>
-					<div class="container">
-					<div class="form-group">
-						<label for="contactLucySubject">Subject</label>
-						<div class="input-group">
-							<div class="input-group-prepend">
-								<span class="input-group-text"></span>
-								<i class="fa fa-comment" aria-hidden="true"></i>
-							</div>
-							<textarea class="form-control" rows="5" id="contactLucyMessage" name="contactLucyMessage" placeholder="I <3 Lucy :D~ (2000 char max)"></textarea>
-						</div>
-					</div>
-					</div>
-<!--			reCaptcha		-->
-					<div class="container">
-					<div class="g-recaptcha" data-sitekey="--6Lfw4LIUAAAAAAeiYeEiumR7prnFi0Lml9wvCWlU--"></div>
-					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i>Send</button>
-					<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i>Reset</button>
-					</form>
-					</div>
-<!--			empty area for form error/success output-->
+			<section class="py-5">
+				<div class="container">
 					<div class="row">
-						<div class="col-xs-12">
-							<div id="output-area"></div>
+						<div class="col-lg-4 offset lg-4">
+							<form id="lucys-demo-form" action="php/mailer.php" method="post">
+								<div class="form-group">
+									<label for="contactLucy">Name <span class="text-danger">*</span></label>
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<span class="input-group-text"></span>
+											<i class="fa fa-user" aria-hidden="true"></i>
+										</div>
+										<input type="text" class="form-control" id="contactLucy" name="contactLucy" placeholder="Lucy">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="contactDemoEmail">Email <span class="text-danger">*</span></label>
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<span class="input-group-text"></span>
+											<i class="fa fa-envelope" aria-hidden="true"></i>
+										</div>
+										<input type="email" class="form-control" id="contactLucyEmail" name="contactLucyEmail" placeholder="Email">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="contactDemoSubject">Subject</label>
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<span class="input-group-text"></span>
+											<i class="fa fa-pencil" aria-hidden="true"></i>
+										</div>
+										<input type="text" class="form-control" id="contactLucySubject" name="contactLucySubject" placeholder="Subject">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="contactDemoMessage">Message <span class="text-danger">*</span></label>
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<span class="input-group-text"></span>
+											<i class="fa fa-comment" aria-hidden="true"></i>
+										</div>
+										<textarea class="form-control" rows="5" id="contactLucyMessage" name="contactLucyMessage" placeholder="I <3 Lucy :D~ (2000 characters max)"></textarea>
+									</div>
+								</div>
+
+								<!-- reCAPTCHA -->
+								<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+								<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+								<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+							</form>
+
+							<!--empty area for form error/success output-->
+							<div class="row">
+								<div class="col-xs-12">
+									<div id="output-area"></div>
+								</div>
+							</div>
+
+
 						</div>
 					</div>
-		</section>
+				</div>
+			</section>
+<
 
 	</body>
 </html>
