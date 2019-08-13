773723b347c8c2084b9590239fbd5267e8277f43
John Dunn <jdunn33@cnm.edu>
2019-08-13 15:26:08 -0600

    adding dependecies for contact form

--- public_html/index.php
+++ public_html/index.php
@@ -17,6 +17,16 @@
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 <!--		FancyBox 3-->
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
 		<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
@@ -106,8 +116,6 @@
 <!--		contact form section-->
 		<section class="py-5">
 			<div class="container">
-				<div class="row">
-					<div class="col-lg-4 offset-lg-4">
 						<form id="Lucys-contact-form" action="php/mailer.php" method="post">
 							<div class="form-group">
 								<label for="contactLucy">Lucy <span class="text-danger">*</span></label>
@@ -130,6 +138,7 @@
 								<input type="email" class="form-control" id="contactLucyEmail" name="contactLucyEmail" placeholder="Email">
 							</div>
 					</div>
+					<div class="container"></div>
 					<div class="form-group">
 						<label for="contactLucySubject">Subject</label>
 						<div class="input-group">
@@ -144,16 +153,13 @@
 					<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
 					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i>Send</button>
 					<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i>Reset</button>
-				</div>
-			</div>
-				</form>
+					</form>
 <!--			empty area for form error/success output-->
 					<div class="row">
 						<div class="col-xs-12">
 							<div id="output-area"></div>
 						</div>
 					</div>
-
 		</section>
 
 	</body>
